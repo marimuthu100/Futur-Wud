2d949c875da32bcab819b8882eb27b436fbf3290
Mohamad Meerasa <mohamadmeerasa45@gmail.com>
2023-01-05 18:50:34 +0530

    Pre FInal Commit

--- js/script.js
+++ js/script.js
@@ -782,3 +782,11 @@
     sortableMasonry();
   });
 })(window.jQuery);
+
+/*let docTitle = document.title;
+window.addEventListener("blur" , () => {
+  document.title = "Come Back :(";
+})
+window.addEventListener("focus", () =>{
+  document.title = docTitle;
+})*/
